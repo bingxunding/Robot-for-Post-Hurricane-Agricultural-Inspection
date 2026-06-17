493bf54229a2e9307e2a020b3bf466d9f4617c59
Luigi Zheng <luigizheng@MacBook-Air-di-Luigi.local>
2026-06-17 11:48:05 +0200

    tweaks on esp32

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -9,24 +9,32 @@ const char* password = "12345ciaooo";
 WebServer server(80);
 
 // ---INITIALIZE MOTOR PINS ---///
-const int ENA = 22;   
-const int IN1 = 4;  
-const int IN2 = 2;  
+const int ENA = 13;   
+const int IN1 = 12;  
+const int IN2 = 14;  
 
-const int ENB = 5;   
-const int IN3 = 13; 
-const int IN4 = 21;   
+const int ENB = 27;   
+const int IN3 = 26; 
+const int IN4 = 25;   
+
+// PWM configuration
+#define PWM_FREQ 500        // 500 Hz for reliable low speeds
+#define PWM_RES 8           // 8-bit resolution (0-255)
+
+// Define PWM channels
+const int pwmChannelA = 0;
+const int pwmChannelB = 1;
 
 int motorSpeed = 255;  
-int curvaturePercentage = 20;
+int curvaturePercentage = 100;
 
 void move_forward() {
-  digitalWrite(IN1, HIGH);     
-  digitalWrite(IN2, LOW);  
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, motorSpeed);
-  analogWrite(ENB, motorSpeed);
+  ledcWrite(pwmChannelA, motorSpeed);
+  ledcWrite(pwmChannelB, motorSpeed);
 }
 
 void move_backward() {
@@ -34,8 +42,8 @@ void move_backward() {
   digitalWrite(IN2, HIGH);  
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, motorSpeed);
-  analogWrite(ENB, motorSpeed);
+  ledcWrite(pwmChannelA, motorSpeed);
+  ledcWrite(pwmChannelB, motorSpeed);
 }
 
 void turn_left() {
@@ -43,8 +51,8 @@ void turn_left() {
   digitalWrite(IN2, HIGH);  
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, motorSpeed);
-  analogWrite(ENB, motorSpeed);
+  ledcWrite(pwmChannelA, motorSpeed);
+  ledcWrite(pwmChannelB, motorSpeed);
 }
 
 void turn_right() {
@@ -53,35 +61,31 @@ void turn_right() {
   digitalWrite(IN2, LOW);  
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, motorSpeed);
-  analogWrite(ENB, motorSpeed);
+  ledcWrite(pwmChannelA, motorSpeed);
+  ledcWrite(pwmChannelB, motorSpeed);
 }
 
 // supposing the rightmotor is ENA
+// Gentle left turn: left motor slower, right motor full speed
 void turn_left_slowly() {
-
-  int internalWheelSpeed = motorSpeed * (100 - curvaturePercentage) / 100;
-
-  digitalWrite(IN1, HIGH);     
-  digitalWrite(IN2, LOW);  
+  int leftSpeed = motorSpeed * (100 - curvaturePercentage) / 100;
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, internalWheelSpeed);  
-  analogWrite(ENB, motorSpeed);
+  ledcWrite(pwmChannelA, motorSpeed);   // left slower
+  ledcWrite(pwmChannelB, leftSpeed);  // right full
 }
 
-// supposing the rightmotor is ENA
+// Gentle right turn: right motor slower, left motor full speed
 void turn_right_slowly() {
-
-  int internalWheelSpeed = motorSpeed * (100 - curvaturePercentage) / 100;
-
-  digitalWrite(IN1, HIGH);     
-  digitalWrite(IN2, LOW);  
+  int rightSpeed = motorSpeed * (100 - curvaturePercentage) / 100;
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, motorSpeed);
-  analogWrite(ENB, internalWheelSpeed);
-
+  ledcWrite(pwmChannelA, rightSpeed);  // left full
+  ledcWrite(pwmChannelB, motorSpeed);  // right slower
 }
 
 void stop_motors() {
@@ -90,10 +94,10 @@ void stop_motors() {
   digitalWrite(IN2, LOW);  
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
-}
+  ledcWrite(pwmChannelA, 0); 
+  ledcWrite(pwmChannelB, 0);
 
+}
 void initialize_HTTP_commands () {
   // --- Definizione degli endpoint (i "comandi" via web)---
   server.on("/on", []() {
@@ -135,19 +139,62 @@ void initialize_HTTP_commands () {
     stop_motors();
     server.send(200, "text/plain", "stop");
   });
+  server.on("/setspeed", []() {
+  if (server.hasArg("value")) {
+    int newSpeed = server.arg("value").toInt();
+    // Clamp between 0 and 255
+    if (newSpeed < 0) newSpeed = 0;
+    if (newSpeed > 255) newSpeed = 255;
+    motorSpeed = newSpeed;
+    Serial.print("Motor speed set to: ");
+    Serial.println(motorSpeed);
+    server.send(200, "text/plain", "Speed set to " + String(motorSpeed));
+  } else {
+    server.send(400, "text/plain", "Missing 'value' parameter");
+  }
+});
+
+server.on("/setcurvature", []() {
+  if (server.hasArg("value")) {
+    int newCurve = server.arg("value").toInt();
+    // Clamp between 0 and 100
+    if (newCurve < 0) newCurve = 0;
+    if (newCurve > 100) newCurve = 100;
+    curvaturePercentage = newCurve;
+    Serial.print("Curvature percentage set to: ");
+    Serial.println(curvaturePercentage);
+    server.send(200, "text/plain", "Curvature set to " + String(curvaturePercentage));
+  } else {
+    server.send(400, "text/plain", "Missing 'value' parameter");
+  }
+});
   server.onNotFound([]() {
     server.send(404, "text/plain", "Endpoint non trovato");
     Serial.println("Richiesta non valida: " + server.uri());
 });
 }
+void setup_pins() {
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
+    // Setup PWM channels
+  ledcSetup(pwmChannelA, PWM_FREQ, PWM_RES);
+  ledcAttachPin(ENA, pwmChannelA);
+  ledcSetup(pwmChannelB, PWM_FREQ, PWM_RES);
+  ledcAttachPin(ENB, pwmChannelB);
+}
 void setup() {
   Serial.begin(9600);
-
+  setup_pins();   
   // Connessione al Wi-Fi
   Serial.print("Connessione a ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
